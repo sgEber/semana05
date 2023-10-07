8dfa75b649baf75b9ace70124470c3235a6f91cb
sgEber <eber.solano@tecsup.edu.pe>
2023-10-07 12:35:44 -0500

    LabelDemo y LabelCode modificados

--- semana05/semana05/LabelCode.xaml.cs
+++ semana05/semana05/LabelCode.xaml.cs
@@ -15,6 +15,33 @@ namespace semana05
         public LabelCode()
         {
             InitializeComponent();
+
+            StackLayout stack = new StackLayout();
+
+            var UnderlineLabel = new Label
+            {
+                Text = "This is underlined text.",
+                TextDecorations = TextDecorations.Underline
+            };
+
+            var strikethroughLabel = new Label
+            {
+                Text = "This is text with strikethroughLabel.",
+                TextDecorations = TextDecorations.Strikethrough
+            };
+
+            var bothLabel = new Label
+            {
+                Text = "This is underlined text with strikethrough. ",
+                TextDecorations = TextDecorations.Underline | TextDecorations.Strikethrough
+            };
+
+            stack.Children.Add(UnderlineLabel);
+            stack.Children.Add(strikethroughLabel);
+            stack.Children.Add(bothLabel);
+
+            Content = stack;
+
         }
     }
 }
